8a551fceeacfa236735162705c9dc778cfbd837d
JuanDa <robayoolaya@gmail.com>
2025-09-06 17:10:27 -0500

    Agregando PDFUtil

--- src/main/java/com/PdfMaker/util/PdfResponseBuilder.java
+++ src/main/java/com/PdfMaker/util/PdfResponseBuilder.java
@@ -1,7 +1,17 @@
 package com.PdfMaker.util;
 
+import com.PdfMaker.model.ResponseContent;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PdfResponseBuilder {
+    public static ResponseEntity<byte[]> build(ResponseContent responseContent){
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=" + responseContent.fileName())
+                .contentType(responseContent.contentType() != null ? responseContent.contentType() : MediaType.APPLICATION_PDF)
+                .body(responseContent.content());
+    }
 }
--- src/main/java/com/PdfMaker/util/PdfUtil.java
+++ src/main/java/com/PdfMaker/util/PdfUtil.java
@@ -1,7 +1,41 @@
 package com.PdfMaker.util;
 
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 
 @Component
 public class PdfUtil {
+    private final TemplateEngine templateEngine;
+
+
+    public PdfUtil(TemplateEngine templateEngine) {
+        this.templateEngine = templateEngine;
+    }
+
+    public String renderHtml(String templateName, Context context){
+        return templateEngine.process(templateName, context);
+    }
+
+    public byte[] generatePdfFromHtml(String htmlContent, String basePath) {
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()){
+            PdfRendererBuilder builder = new PdfRendererBuilder();
+            builder.withHtmlContent(htmlContent, basePath);
+            builder.toStream(outputStream);
+            builder.run();
+            return outputStream.toByteArray();
+        } catch (IOException e){
+            throw new RuntimeException("Error al generara el PDF", e);
+        }
+    }
+
+    public byte[] generatePdf(String templateName, Context context) {
+        String htmlContent = renderHtml(templateName, context);
+        String baseUrl = new File("src/main/resources/static/").toURI().toString();
+        return generatePdfFromHtml(htmlContent, baseUrl);
+    }
 }
