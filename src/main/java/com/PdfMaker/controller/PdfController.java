d0aef1cdd0afc34838ff34d247c7333219f9d4eb
JuanDa <robayoolaya@gmail.com>
2025-09-06 17:18:08 -0500

    Crear PdfService

--- src/main/java/com/PdfMaker/controller/PdfController.java
+++ src/main/java/com/PdfMaker/controller/PdfController.java
@@ -1,14 +1,21 @@
 package com.PdfMaker.controller;
 
+import com.PdfMaker.service.PdfService;
+import com.PdfMaker.util.PdfResponseBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PdfController {
+    private final PdfService pdfService;
+
+    public PdfController(PdfService pdfService) {
+        this.pdfService = pdfService;
+    }
 
     @GetMapping("/generate")
     public ResponseEntity<byte[]> generatePdfTax(){
-        return
+        return PdfResponseBuilder.build(PdfService.generatePdf());
     }
 }
--- src/main/java/com/PdfMaker/service/PdfService.java
+++ src/main/java/com/PdfMaker/service/PdfService.java
@@ -1,9 +1,35 @@
 package com.PdfMaker.service;
 
 
+import com.PdfMaker.model.ResponseContent;
+import com.PdfMaker.model.Tax;
+import com.PdfMaker.util.PdfUtil;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 
 @Service
 public class PdfService {
+    private final PdfUtil pdfUtil;
 
+    public PdfService(PdfUtil pdfUtil) {
+        this.pdfUtil = pdfUtil;
+    }
+
+    public ResponseContent generatePdf(){
+        Tax tax = new Tax(
+
+        );
+
+        Context context = new Context();
+        context.setVariable("taxtManager", tax);
+
+        byte[] pdfBytes = pdfUtil.generatePdf("tax", context);
+
+        return new ResponseContent(
+                pdfBytes,
+                "Factura_Tax.pdf",
+                MediaType.APPLICATION_PDF
+        );
+    }
 }
